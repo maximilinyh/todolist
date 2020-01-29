3d4cd620d7e114db72cac0765c4959aa29b4f78b
maximilinyh <maxim_ilinyh@mail.ru>
2020-01-29 15:00:19 +0700

    bugfix

---
+++ dist/js/2.3a1553f5083f7e6141c3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{117:function(e,o){[Element.prototype,CharacterData.prototype,DocumentType.prototype].filter(Boolean).forEach((function(e){e.hasOwnProperty("remove")||Object.defineProperty(e,"remove",{configurable:!0,enumerable:!0,writable:!0,value:function(){this.parentNode&&this.parentNode.removeChild(this)}})}))}}]);
+//# sourceMappingURL=2.3a1553f5083f7e6141c3.js.map
--- dist/js/2.a5c574b30f1225a0281d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{118:function(e,o){[Element.prototype,CharacterData.prototype,DocumentType.prototype].filter(Boolean).forEach((function(e){e.hasOwnProperty("remove")||Object.defineProperty(e,"remove",{configurable:!0,enumerable:!0,writable:!0,value:function(){this.parentNode&&this.parentNode.removeChild(this)}})}))}}]);
-//# sourceMappingURL=2.a5c574b30f1225a0281d.js.map
